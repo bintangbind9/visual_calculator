1a1cdcdff40f5ec0c2788119971d233b4f690613
bintangbind9 <bintangsaputra531@gmail.com>
2024-09-22 03:09:31 +0700

    Add Launcher Icon

--- lib/src/common/constant/app_constant.dart
+++ lib/src/common/constant/app_constant.dart
@@ -8,6 +8,8 @@ class AppConstant {
   static const double maxDialogWidth = 400;
   static const double smallScreenMaxWidth = 480;
   static const double mediumScreenMaxWidth = 1024;
+
+  // TODO: Store to flutter_secure_storage or config!
   static const String secretKey =
       'JP+QNZOA1YVcQ8jqlHnCDsN+k77fKYA6vhtWbWHXceU=';
   static const String secretIV = 'x76GGAAgRKlo916Zr7gWWw==';
